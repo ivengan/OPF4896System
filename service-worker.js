1ae23e8b0d8ce64e45bbe9c156516077c4729057
ivengan <ivengan@users.noreply.github.com>
2026-04-17 02:25:39 +0000

    deploy: b847c4468601a2d138434194f3703f78079e0bf5

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 // D:\OPF4896System\service-worker.js
 
-const CACHE_NAME = 'opf-system-v9';
+const CACHE_NAME = 'opf-system-v10';
 // List all files you want to work offline
 const ASSETS = [
   '.',
