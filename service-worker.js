a0684ceb0ab6b57622f990ae0713da48b7daaf35
OPF4896 System <opf4896@system.local>
2026-04-17 09:46:02 +0800

    force PWA update to v3 and unify login modal buttons to Aquamarine

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 // D:\OPF4896System\service-worker.js
 
-const CACHE_NAME = 'opf-system-v2';
+const CACHE_NAME = 'opf-system-v3';
 // List all files you want to work offline
 const ASSETS = [
   '.',
